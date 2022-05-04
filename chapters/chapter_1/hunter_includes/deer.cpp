da244f95ffe54fff07d2eb3e69687fd1da198ba7
aksai4in <ak290802@connect.hku.hk>
2022-05-04 11:26:18 +0600

    huter.cpp finished
    fineshed hunter.cpp and added knight storyline.

---
+++ chapters/chapter_1/hunter_includes/deer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void deer(){
+    cout << " - Deer are pretty difficult to find these days. So I don't expect much from you.\n";
+    cin.get();
+    cout << " - But the pay is good if you do kill one\n";
+    cin.get():
+    cout << " - I would suggest you go the north side of the ...\n";
+    cin.get();
+    cout << "After leaving Burnson, you head to the forest to hunt deer\n";
+    cin.get();
+    cout << "You search for the whole day and are able to find one deer only very late at night\n";
+    cin.get();
+    cout << "You kill it and head back to Burnson\n";
+    cin.get();
+}
---
+++ chapters/chapter_1/hunter_includes/goes_to_forest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void goes_to_forest(){
+    cout << " - Well, I tried warning you. |sighs| (Burnson)\n";
+    cin.get();
+    cout << "After leaving the village, you a band of goblins attacks you.\n";
+    cin.get();
+    cout << "You are outnumbered, and there is noway to escape.\n";
+    cin.get();
+    cout << "Goblins are able to defeat you.\n\n\n";
+    cout << "GAME OVER\n";
+}
