6cd76239e6e39973d4f2a01ea5496b947b361194
rooktc0917 <u3560171@hku.hk>
2022-05-06 16:01:42 +0800

    Increase variety of monsters
    Number of monsters possible to encounter with the player is increased so as to increase the randomness of each gameplay

--- chapters/chapter_2/thief_includes/monster_includes/rand_monster.cpp
+++ chapters/chapter_2/thief_includes/monster_includes/rand_monster.cpp
@@ -5,17 +5,73 @@
 using namespace std;
 void rand_monster(monster *monster){
     srand(time(0));
-    int type = rand() % 2;
+    int type = rand() % 10;
     if(type == 0){
         monster->name = "Monster wolf";
-        monster->level = 5;
+        monster->strength = 15;
         monster->luck = 3;
-        monster->HP = 100;
+        monster->HP = 20;
     }
     else if(type == 1){
         monster->name = "Giant Ant";
-        monster->level = 7;
-        monster->luck = 4;
-        monster->HP = 100;
+        monster->strength = 10;
+        monster->luck = 6;
+        monster->HP = 15;
+    }
+
+    else if(type == 2){
+        monster->name = "Black Rat";
+        monster->strength = 10;
+        monster->luck = 3;
+        monster->HP = 15;
+    }
+
+    else if(type == 3){
+        monster->name = "Dark Bat";
+        monster->strength = 8;
+        monster->luck = 8;
+        monster->HP = 10;
+    }
+
+    else if(type == 4){
+        monster->name = "Charging Boar";
+        monster->strength = 20;
+        monster->luck = 1;
+        monster->HP = 20;
+    }
+
+    else if(type == 5){
+        monster->name = "Troll";
+        monster->strength = 20;
+        monster->luck = 1;
+        monster->HP = 20;
+    }
+
+    else if(type == 6){
+        monster->name = "Mutated Alligator";
+        monster->strength = 20;
+        monster->luck = 3;
+        monster->HP = 15;
+    }
+
+    else if(type == 7){
+        monster->name = "Basilisk";
+        monster->strength = 15;
+        monster->luck = 5;
+        monster->HP = 10;
+    }
+
+    else if(type == 8){
+        monster->name = "Underground Worm";
+        monster->strength = 10;
+        monster->luck = 2;
+        monster->HP = 20;
+    }
+
+    else if(type == 9){
+        monster->name = "Cursed Knight";
+        monster->strength = 20;
+        monster->luck = 5;
+        monster->HP = 20;
     }
 }
