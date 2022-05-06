3be4492b4b9cfdb857dc7c8c198d74fd6c88977c
rooktc0917 <u3560171@hku.hk>
2022-05-06 22:58:47 +0800

    Put the function of fight_monster into a separate file

--- chapters/chapter_2/thief_includes/monster.cpp
+++ chapters/chapter_2/thief_includes/monster.cpp
@@ -1,67 +1,8 @@
 #include <iostream>
-//#include "monster_includes/fight_monster.cpp"
-#include "monster_includes/run_away_monster.cpp"
 #include "monster_includes/rand_monster.cpp"
+#include "monster_includes/fight_monster.cpp"
+#include "monster_includes/run_away_monster.cpp"
 using namespace std;
-
-void fight_monster(monster_attribute *monster, attribute *player){
-    srand(time(0));
-
-    if (player->role == "Warrior"){
-        cout << "***You draw your sword out and prepare to fight " << monster->name << ".***\n";
-        cin.get();
-    }
-
-    else if (player->role == "Archer"){
-        cout << "***You draw an arrow out and prepare to fight " << monster->name << ".***\n";
-        cin.get();
-    }
-
-    else (player->role == "Mage"){
-        cout << "***You draw your wand out and prepare to fight " << monster->name << ".***\n";
-        cin.get();
-    }
-
-
-
-    while (monster->HP > 0 && player->HP > 0){
-        cout << "***Your HP now is " << player->HP << ".***\n\n";
-        cout << "***" << monster->name << " HP now is " << monster->HP << ".***\n\n";
-        cout << "***What do you want to do?***\n";
-
-        cout << "***You attack " << monster->name << "!***\n";
-
-        monster->HP = monster->HP - player->strength;
-            
-        if (rand()%10 < player->luck){
-            cout <<"\n***You successfully deliver extra damage!***\n";
-            monster->HP = monster->HP - player->strength*0.5;
-        }
-
-        cin.get();
-
-        cout << "***" << monster->name << " attack you!***\n";
-
-        player->HP = player->HP - monster->strength;
-            
-        if (rand()%10 < monster->luck){
-            cout <<"\n***Monster delivers extra damage!***\n";
-            player->HP = player->HP - monster->strength*0.5;
-        }
-
-        cin.get();
-    }
-
-
-    while (monster->HP > 0 && player->HP > 0){
-        
-    };
-
-    cout << "***You defeated the monster!***\n";
-    cin.get();
-    cout << "***You continue your travel***\n";
-    cin.get();
-}
 
 void monster_encounter(attribute *player){
     monster_attribute *ptr_monster;
